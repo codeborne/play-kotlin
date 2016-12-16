60e0c130ecadee206adfc33836d8c263b675ccea
Anton Keks <anton@azib.net>
2016-12-16 19:35:48 +0200

    rename to avoid conflicts

--- app/controllers/App.kt
+++ app/controllers/App.kt
@@ -4,7 +4,7 @@ import models.User
 import play.db.jpa.JPA
 import play.mvc.Controller
 
-class Application : Controller() {
+class App : Controller() {
   fun index(param: String?) {
     val user = JPA.em().find(User::class.java, 1L)
     render(param, user)
