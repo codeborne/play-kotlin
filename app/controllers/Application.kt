44da6a9294710e555bcaedef964d4328eebcf993
Anton Keks <anton@azib.net>
2016-12-16 14:14:09 +0200

    param needs to be nullable

--- app/controllers/Application.kt
+++ app/controllers/Application.kt
@@ -5,7 +5,7 @@ import play.db.jpa.JPA
 import play.mvc.Controller
 
 class Application : Controller() {
-  fun index(param: String) {
+  fun index(param: String?) {
     val user = JPA.em().find(User::class.java, 1L)
     render(param, user)
   }
