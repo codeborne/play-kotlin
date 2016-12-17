2977be88553e174df8e89fa4a08ce65734f39cb4
Anton Keks <anton@azib.net>
2016-12-17 20:34:44 +0200

    add nicer render method for Kotlin controllers

--- app/controllers/App.kt
+++ app/controllers/App.kt
@@ -7,8 +7,6 @@ import play.kotlin.Controller
 class App : Controller() {
   fun index(param: String?) {
     val user = JPA.em().find(User::class.java, 1L)
-    renderArgs["param"] = param
-    renderArgs["user"] = user
-    render()
+    render("param" to param, "user" to user)
   }
 }
--- app/play/kotlin/Controller.kt
+++ app/play/kotlin/Controller.kt
@@ -4,4 +4,12 @@ import play.mvc.Scope
 
 open class Controller : play.mvc.Controller() {
   operator fun Scope.RenderArgs.set(name: String, value: Any?) = put(name, value)
+
+  fun render(vararg params: Pair<String, Any?>) {
+    renderTemplate(template(), params.toMap())
+  }
+
+  fun render(templateName: String, vararg params: Pair<String, Any?>) {
+    renderTemplate(templateName, params.toMap())
+  }
 }
