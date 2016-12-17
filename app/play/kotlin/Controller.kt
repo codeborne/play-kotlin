076c8c4f141bfd3ca7d1d9137f77f876a5960fc2
Anton Keks <anton@azib.net>
2016-12-17 20:26:51 +0200

    use shorter syntax

--- app/play/kotlin/Controller.kt
+++ app/play/kotlin/Controller.kt
@@ -3,7 +3,5 @@ package play.kotlin
 import play.mvc.Scope
 
 open class Controller : play.mvc.Controller() {
-  operator fun Scope.RenderArgs.set(name: String, value: Any?) {
-    put(name, value)
-  }
+  operator fun Scope.RenderArgs.set(name: String, value: Any?) = put(name, value)
 }
